c73cdf403016fad6648949769f610f826126947c
Alice T <alicewtien@gmail.com>
2018-07-13 00:16:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if board[index] == "X" || board[index] == "O"
+  if index.between?(0, 8) && board[index] == "X" || board[index] == "O"
     return false
   else
     return true
