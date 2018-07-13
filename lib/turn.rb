e46364d6f57990ef18a9e7c6e5f756b6a6b1c7b3
Alice T <alicewtien@gmail.com>
2018-07-13 00:15:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if board[index] == "X" || "O"
+  if board[index] == "X" || board[index] == "O"
     return false
   else
     return true
