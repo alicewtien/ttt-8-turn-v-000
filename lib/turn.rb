3728f29d00c586ea3f9a2999de271ccff2e9776a
Alice T <alicewtien@gmail.com>
2018-07-13 01:02:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,7 +26,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board
+    display_board(board)
   else
     puts "Invalid move"
     turn(board)
